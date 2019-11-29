4447b70f63278de76d38cc4ce01074f947dff414
catdog50rus <catdog50rus@gmail.com>
2019-11-29 16:01:46 +0300

    3dfinal

--- js/main.js
+++ js/main.js
@@ -1,68 +1,74 @@
 const form = $('#price-form');
 let formData = form.serializeJSON();
+
+
 showHideBlocks(formData);
 
-form.on('keyup change', 'input, select, textarea', function(){
-    let formData = form.serializeJSON();
-    //console.log(formData);
+$('#total-price').text(getTotalPrice(formData));
 
+form.on('keyup change', 'input, select, textarea', function(){
+    formData = form.serializeJSON();
+    
     showHideBlocks(formData);
 
-   // getTotalPrice(formData);
-
-    //console.log(totalPrice);
-
-    $('#total-price').text( getTotalPrice(formData) );
+    $('#total-price').text(getTotalPrice(formData));
 });
 
-function  showHideBlocks(formData){
+function showHideBlocks() {
+    
     if (formData.type == 'site') {
         $('[data-name="landing"]').hide();
         $('[data-name="pages"]').show();
-    } else {
+        formData['sections'] = 0;
+    } 
+    else if (formData.type == 'landing') {
         $('[data-name="pages"]').hide();
         $('[data-name="landing"]').show();
+        formData['pages-unique'] = 0;
+        formData['pages-general'] = 0;
     };
+
 
     if (formData.mobile == 'on') {
         $('[data-name="mobile"]').show();
-    } else {
+    } 
+    else {
         $('[data-name="mobile"]').hide();
         $('[name="mobile-number"]')[0].checked = true;
         $('[name="mobile-number"]')[1].checked = false;
         $('[name="mobile-number"]')[2].checked = false;
+        formData['mobile-number'] = 1;
+
     }
+
 
 };
 
 
 function getTotalPrice(formData){
     let totalPrice = 0;
-
+    
     totalPrice =
         formData['pages-unique'] * 4000 +
         formData['pages-general'] * 2500 +
         formData['sections'] * 2000 +
-        formData['mobile-number'] * 1 +
         formData['carousel'] * 1200 +
         formData['modals'] * 900 +
         formData['forms'] * 1500;
-
+  
+    totalPrice = (totalPrice * GetK(formData));
     
-    totalPrice = totalPrice * GetK(formData);
-    console.log(totalPrice);
-    
-    return totalPrice;
-
+    return totalPrice.toFixed(0);
 };
 
 function GetK(formData) {
     // адаптив
     let kMobile = 1;
+    
 
-    if (formData['mobile-number'] == 1) {
+    if (formData['mobile-number'] == 2) {
         kMobile = 1.3;
-    } else if (formData['mobile-number'] == 2) {
+    } else if (formData['mobile-number'] == 3) {
         kMobile = 1.5;
     }
 
@@ -88,10 +94,13 @@ function GetK(formData) {
     }
 
     let k = 1;
-    k = kMobile *
+    k = kMobile *  
         kPixelPerfect *
         kRetinaReady *
         kGooglePageSpeed *
         kUrgentOrder;
+    
+    
+    console.log('k: ', k);
     return k;
 };
